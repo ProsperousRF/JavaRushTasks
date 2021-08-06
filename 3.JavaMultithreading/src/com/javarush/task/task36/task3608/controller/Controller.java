1b4bb379d5c8b501aa3a49c8cacd7ca38e95b26e
Stanislav Rakitov <prosperousrf@gmail.com>
2021-08-06 19:17:55 +0300

    MVC. MVC (3)

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task36.task3608.controller;
 
 import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.UsersView;
 
 /**
  * @author Stanislav Rakitov
@@ -8,12 +9,19 @@ import com.javarush.task.task36.task3608.model.Model;
 public class Controller {
 
   private Model model;
+  private UsersView usersView;
 
   public void setModel(Model model) {
     this.model = model;
   }
 
   public void onShowAllUsers(){
+
     model.loadUsers();
+    usersView.refresh(model.getModelData());
+  }
+
+  public void setUsersView(UsersView usersView) {
+    this.usersView = usersView;
   }
 }
