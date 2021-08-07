e44137abbed97f5e2657047d4c0dfd9f1df48039
Stanislav Rakitov <prosperousrf@gmail.com>
2021-08-07 22:07:57 +0300

    MVC. UsersView

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task36.task3608.view;
 
+import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.controller.Controller;
 import com.javarush.task.task36.task3608.model.ModelData;
 
@@ -8,25 +9,27 @@ import com.javarush.task.task36.task3608.model.ModelData;
  */
 public class UsersView implements View{
 
-  private Controller controller;
+    private Controller controller;
 
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("All users:");
+        for (User user : modelData.getUsers()) {
+            System.out.println("\t" + user);
+        }
+        System.out.println("==================================================");
+    }
 
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
 
-  @Override
-  public void refresh(ModelData modelData) {
-    System.out.println("All users:");
-    modelData.getUsers().forEach(user -> System.out.println("\t" + user));
-    System.out.println("===================================================");
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
 
-  }
-
-  @Override
-  public void setController(Controller controller) {
-    this.controller = controller;
-  }
-
-  public void fireEventShowAllUsers(){
-    controller.onShowAllUsers();
-  }
-
+    public void fireEventShowDeletedUsers() {
+        controller.onShowAllDeletedUsers();
+    }
 }
