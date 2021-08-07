d1c712efb6aa63b000e68441f21a51af3af8a504
Stanislav Rakitov <prosperousrf@gmail.com>
2021-08-07 22:08:35 +0300

    MVC. Controller

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/controller/Controller.java
@@ -8,20 +8,24 @@ import com.javarush.task.task36.task3608.view.UsersView;
  */
 public class Controller {
 
-  private Model model;
-  private UsersView usersView;
+    private Model model;
+    private UsersView usersView;
 
-  public void setModel(Model model) {
-    this.model = model;
-  }
+    public void setModel(Model model) {
+        this.model = model;
+    }
 
-  public void onShowAllUsers(){
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
 
-    model.loadUsers();
-    usersView.refresh(model.getModelData());
-  }
+    public void onShowAllUsers() {
+        model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
 
-  public void setUsersView(UsersView usersView) {
-    this.usersView = usersView;
-  }
+    public void onShowAllDeletedUsers() {
+        model.loadDeletedUsers();
+        usersView.refresh(model.getModelData());
+    }
 }
