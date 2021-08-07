f17b7349ef3b9f2c3847f2a49961876393f1dae1
Stanislav Rakitov <prosperousrf@gmail.com>
2021-08-07 22:06:37 +0300

    MVC. MainModel

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -11,17 +11,28 @@ import java.util.List;
  */
 public class MainModel implements Model{
 
-  private ModelData modelData = new ModelData();
-  private UserService userService = new UserServiceImpl();
+    //use helpful services
+    private UserService userService = new UserServiceImpl();
 
-  @Override
-  public ModelData getModelData() {
-    return modelData;
-  }
+    //use special object to keep data for view rendering
+    private ModelData modelData = new ModelData();
 
-  @Override
-  public void loadUsers() {
-    List<User> usersBetweenLevels = userService.getUsersBetweenLevels(1, 100);
-    modelData.setUsers(usersBetweenLevels);
-  }
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = userService.getUsersBetweenLevels(1, 100);
+        //refresh model data
+        modelData.setUsers(users);
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        List<User> users = userService.getAllDeletedUsers();
+        //refresh model data
+        modelData.setUsers(users);
+    }
 }
