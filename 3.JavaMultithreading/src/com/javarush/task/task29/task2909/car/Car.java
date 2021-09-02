2173a093c215d0c1c8b58b39ec56e51151b303ed
Stanislav Rakitov <prosperousrf@gmail.com>
2021-09-02 16:30:27 +0300

    task2909. Рефакторинг 12

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Car.java
@@ -2,10 +2,13 @@ package com.javarush.task.task29.task2909.car;
 
 import java.util.Date;
 
-public class Car {
+public abstract class Car {
   public static final int TRUCK = 0;
   public static final int SEDAN = 1;
   public static final int CABRIOLET = 2;
+  public static final int MAX_TRUCK_SPEED = 80;
+  public static final int MAX_SEDAN_SPEED = 120;
+  public static final int MAX_CABRIOLET_SPEED = 90;
   public double summerFuelConsumption;
   public double winterFuelConsumption;
   public double winterWarmingUp;
@@ -46,10 +49,11 @@ public class Car {
   }
 
   public int getNumberOfPassengersCanBeTransferred() {
-    if (!isDriverAvailable()) return 0;
-    if (fuel <= 0) return 0;
-
-    return numberOfPassengers;
+    if (canPassengersBeTransferred()) {
+      return numberOfPassengers;
+    } else {
+      return 0;
+    }
   }
 
   public boolean isDriverAvailable() {
@@ -61,11 +65,9 @@ public class Car {
   }
 
   public void startMoving() {
+      fastenDriverBelt();
     if (numberOfPassengers > 0) {
       fastenPassengersBelts();
-      fastenDriverBelt();
-    } else {
-      fastenDriverBelt();
     }
   }
 
@@ -73,11 +75,7 @@ public class Car {
 
   public void fastenDriverBelt() {}
 
-  public int getMaxSpeed() {
-    if (type == TRUCK) return 80;
-    if (type == SEDAN) return 120;
-    return 90;
-  }
+  public abstract int getMaxSpeed();
 
   public double getWinterConsumption(int length) {
     return length * winterFuelConsumption + winterWarmingUp;
@@ -91,5 +89,9 @@ public class Car {
     return isSummer(date, SummerStart, SummerEnd)
         ? getSummerConsumption(length)
         : getWinterConsumption(length);
+  }
+
+  private boolean canPassengersBeTransferred() {
+    return isDriverAvailable() && fuel > 0;
   }
 }
